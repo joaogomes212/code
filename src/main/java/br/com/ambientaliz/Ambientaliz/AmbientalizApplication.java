35f8e7b255c9ddb2c1c7355bd25619dc20de4ae1
João Gomes <148020237+joaogomes212@users.noreply.github.com>
2023-10-15 23:46:39 -0300

    feat: add init project

---
+++ src/main/java/br/com/ambientaliz/Ambientaliz/AmbientalizApplication.java
@@ -0,0 +1,13 @@
+package br.com.ambientaliz.Ambientaliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbientalizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbientalizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ambientaliz/Ambientaliz/AmbientalizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ambientaliz.Ambientaliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbientalizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
